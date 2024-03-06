00a647b6362acc58b6145a73c62f9c0b31c1b4e9
Hong Ray <119933763+HongRay@users.noreply.github.com>
2024-03-06 21:11:01 +0800

    Tutorial 2
    Did tutorial 2 as instructed.
    Followed instructions but still alittle bit unclear as to how this things work.
    
    Need more time to study this.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -41,6 +41,9 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private FlowPane tags;
 
+    @FXML
+    private Label remark;
+
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
@@ -49,6 +52,7 @@ public class PersonCard extends UiPart<Region> {
         this.person = person;
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
+        remark.setText(person.getRemark().value);
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
